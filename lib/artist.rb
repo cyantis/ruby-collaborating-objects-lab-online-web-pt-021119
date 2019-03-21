2ee1cf2beeee29b1e9cde056e10ee7fa08b6bf9c
Cody Yantis <cyantis@gmail.com>
2019-03-21 00:31:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,11 +13,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.name.include?(name)
-      name
-    else
-      artist = self.new(name)
-    end
+    self.all.find {|n| n.name == name} ? name : artist = self.new(name)
   end
 
   def songs
