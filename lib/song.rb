5c34711be1ff6f137f3a5d85bc2142c874a71700
Cody Yantis <cyantis@gmail.com>
2019-03-21 01:18:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,7 @@ class Song
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
     new_song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    new_song.artist = artist
-    artist.add_song(self)
+    new_song.artist_name = artist_name
     new_song
   end
 
